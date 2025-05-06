3f566e698e7d0fa67621ab71f79e8d5fc01701ef
LucasVince <lucashenriquevince1714@gmail.com>
2025-05-06 18:22:49 -0300

    feat added logs to the api

--- src/controller/register-user/register-user-controller.ts
+++ src/controller/register-user/register-user-controller.ts
@@ -5,10 +5,12 @@ import { iRegisterUserParams, iRegisterUserRepositoy } from './protocols';
 import { isEmail } from 'validator';
 import { hash } from 'bcrypt';
 import { generateToken } from '../helpers';
+import { logger } from '../../utils/logger';
 
 export class registerUserController implements iController {
     constructor(private readonly registerUserRepository: iRegisterUserRepositoy) {}
     async handle(HttpRequest: HttpRequest<iRegisterUserParams>): Promise<HttpResponse<user>> {
+        logger.info('Register user controller initialized');
         try {
             const requiredFields: Array<keyof iRegisterUserParams> = [
                 'name',
@@ -19,31 +21,44 @@ export class registerUserController implements iController {
 
             const body = HttpRequest?.body!;
 
+            logger.info('Register user controller', body);
+
             if (!body) {
+                logger.error('Missing body in register user controller');
                 return badRequest('Missing body');
             }
 
             for (const field of requiredFields) {
                 if (!body[field]) {
+                    logger.error(`Missing param: ${field}`);
                     return badRequest(`Missing param: ${field}`);
                 }
             }
 
             const { name, email, password, role } = body;
 
+            logger.info('name', name);
+            logger.info('email', email);
+            logger.info('password', password);
+            logger.info('role', role);
+
             const isEmailValid = isEmail(email);
 
             if (!isEmailValid) {
+                logger.error('Invalid email format', { email });
                 return badRequest('Invalid email format');
             }
 
             const validRoles = ['teacher', 'student'];
 
-            if (!validRoles.includes(HttpRequest?.body!.role.toLocaleUpperCase())) {
+            if (!validRoles.includes(HttpRequest?.body!.role.toLowerCase())) {
+                logger.error('Invalid role. Role must be either "teacher" or "student"');
                 return badRequest('Invalid role. Role must be either "teacher" or "student"');
             }
 
             const hashedPassword = await hash(password, 10);
+
+            logger.info('Hashed password', hashedPassword);
 
             const userToCreate: iRegisterUserParams = {
                 name: name,
@@ -56,6 +71,9 @@ export class registerUserController implements iController {
 
             const token = generateToken(user);
 
+            logger.info('User created', user);
+            logger.info('Token generated', token);
+            
             return created({ user: user, token: token });
         } catch (error) {
             return serverError(error as string);
---
+++ src/utils/logger.ts
@@ -0,0 +1,16 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        }),
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'src/utils/app.log', level: 'info' }),
+        new winston.transports.File({ filename: 'src/utils/error.log', level: 'error' }),
+    ],
+});
