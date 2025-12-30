c5f09c792a9572919ebf0cb6c191c1478ffe818a
LucasVince <lucashenriquevince1714@gmail.com>
2025-12-30 18:56:17 -0300

    fix: fixed updatecourse factory error, adding getcoursebyid dependece

--- src/factories/update-course-factor.ts
+++ src/factories/update-course-factor.ts
@@ -1,9 +1,14 @@
 import { updateCourseController } from '../controllers/update-course/update-course-controller';
+import { mongoGetCourseByIdRepository } from '../repositories/mongo-get-course-by-id';
 import { mongoUpdateCourseRepository } from '../repositories/mongo-update-course';
 
 export const updateCourseFactory = () => {
     const MongoUpdateCourseRepository = new mongoUpdateCourseRepository();
-    const UpdateCourseController = new updateCourseController(MongoUpdateCourseRepository);
+    const MongoGetCourseByIdRepository = new mongoGetCourseByIdRepository();
+    const UpdateCourseController = new updateCourseController(
+        MongoUpdateCourseRepository,
+        MongoGetCourseByIdRepository,
+    );
 
     return UpdateCourseController;
 };
